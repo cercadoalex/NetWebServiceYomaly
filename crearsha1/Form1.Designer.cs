d1cccaa62ba6175886a5ab1353e7f7621c0061bf
YomalyDev <yomalycastillo19@gmail.com>
2025-05-12 12:29:44 -0500

    first commit

---
+++ crearsha1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace crearsha1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(404, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calcula Sha de un Archivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 60);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(566, 130);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(493, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Calcular SHA";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Location = new System.Drawing.Point(29, 32);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(436, 22);
+            this.txtRuta.TabIndex = 3;
+            this.txtRuta.Text = "C:\\Bingo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ruta de la carpeta del sistema";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(37, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Este Proceso puede durar unos Segundos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 252);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRuta);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Calcular SHA del Sistema de Bingos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ crearsha1/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace crearsha1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /// esta ruta debe estar en la base de datos o en un archivo de texto la cosa es que hay que buscar el exe del bingo
+            var RutaPath = @"C:\Link_Tek_SAC";
+
+            byte[] hash;
+            using (Stream stream = File.OpenRead(RutaPath))
+            {
+                hash = SHA1.Create().ComputeHash(stream);
+            }
+            string base64Hash = Convert.ToBase64String(hash);
+            textBox1.Text = base64Hash;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string someString = txtRuta.Text;
+            string[] RutaPath = new string[] { someString };
+
+            textBox1.Text = "";
+            try
+            {
+                if (RutaPath.Length < 1)
+                {
+                    textBox1.Text = "No directory selected probando proeycto yomalui.";
+                    return;
+                }
+
+                string directory = RutaPath[0];
+                if (Directory.Exists(directory))
+                {
+                    // Create a DirectoryInfo object representing the specified directory.
+                    var dir = new DirectoryInfo(directory);
+                    // Get the FileInfo objects for every file in the directory.
+                    FileInfo[] files = dir.GetFiles();
+                    // Initialize a SHA256 hash object.
+                    using (SHA256 mySHA256 = SHA256.Create())
+                    {
+                        // Compute and print the hash values for each file in directory.
+                        foreach (FileInfo fInfo in files)
+                        {
+                            using (FileStream fileStream = fInfo.Open(FileMode.Open))
+                            {
+                                try
+                                {
+
+                                    fileStream.Position = 0;
+                                    byte[] hashValue = mySHA256.ComputeHash(fileStream);
+
+                                    textBox1.Text = PrintByteArray(hashValue);
+                                }
+                                catch (IOException ex)
+                                {
+                                    Console.WriteLine($"I/O Exception: {ex.Message}");
+                                }
+                                catch (UnauthorizedAccessException ex)
+                                {
+                                    Console.WriteLine($"Access Exception: {ex.Message}");
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The directory specified could not be found.");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                textBox1.Text = $"Error: {ex.Message}";
+            }
+          
+        }
+
+        public static string PrintByteArray(byte[] array)
+        {
+            string sha = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+              
+                sha = sha + $"{array[i]:X2}";
+             
+                
+            }
+
+            return sha;
+        }
+
+    }
+}
